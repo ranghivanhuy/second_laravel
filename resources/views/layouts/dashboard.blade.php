31838f178b1f4e1d4cd9561b110018f8093ab87a
ranghivanhuy <ranghivanhuy@gmail.com>
2020-10-01 11:46:56 +0700

    feature/post: update done

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -16,7 +16,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Dashboard</a>
+        <a class="navbar-brand" href="{{route('dashboard')}}">Dashboard</a>
     </div>
 
     <div class="collapse navbar-collapse navbar-ex1-collapse">
